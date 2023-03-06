44026791183d0f1500547ec2d1da7125cb3b76a3
Fuzzy <chipercualexandru@gmail.com>
2023-03-06 12:35:41 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Table table = new Table(18, 17);
+
+        table.addParam(" border=1 align=top");
+        String src = "https://img.imgsmail.ru/static.promo/logo/logo_white.svg";
+
+        for (Row r: table.getRows()){
+            for (int i = 0; i < r.getCols().size(); i++) {
+                Col col = r.getColByID(i);
+                if (i == 0){
+                    col.setContent(new Img(src).getImgHTML());
+                }else {
+                    col.setContent(i + "");
+                }
+            }
+
+        }
+
+//
+//        table.getRowById(1).getColByID(1).setContent("ASDASDASDASD").addParam(" height=10");
+//        Img img = new Img(src, 64, 64);
+//        table.getRowById(1).getColByID(0).setContent(img.toString());
+
+        String s = table.printTable();
+        System.out.println(s);
+
+
+    }
+}
